8a402188424085e1f8c03f5a811db5bb9d3c35b3
liuyi <liuyiinhhu@gmail.com>
2025-07-23 23:42:01 +0800

    init app

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+const config = {
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 120,
+    proseWrap: 'never',
+    endOfLine: 'auto',
+    semi: true,
+    tabWidth: 4,
+    htmlWhitespaceSensitivity: 'strict',
+};
+export default config;
