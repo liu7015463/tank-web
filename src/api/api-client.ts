466db6f32b94ad3b48c57064be7ab0b3ee3ed952
liuyi <liuyiinhhu@gmail.com>
2025-08-06 10:57:28 +0800

    refactor(api-client): 重构 API 客户端的用户认证和状态管理
    - 使用 useUserToken 和 useUserActions 替代直接访问 userStore
    - 在请求拦截器中添加用户访问令牌
    - 更新响应拦截器中的用户信息和令牌清除逻辑

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import type { Result } from '@/types/api';
 
-import userStore from '@/store/user-store';
+import { useUserActions, useUserToken } from '@/store/user-store';
 import { ResultStats } from '@/types/enum';
 
 const axiosInstance = axios.create({
@@ -17,7 +17,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
     (config) => {
-        config.headers.Authorization = `Bearer Token`;
+        config.headers.Authorization = `Bearer ${useUserToken().accessToken}`;
         return config;
     },
     (err) => Promise.reject(err),
@@ -40,7 +40,7 @@ axiosInstance.interceptors.response.use(
         const errMsg = response?.data?.message || message || '操作失败,系统异常!';
         console.error(errMsg);
         if (response?.status === 401) {
-            userStore.getState().actions.clearUserInfoAndToken();
+            useUserActions().clearUserInfoAndToken();
         }
         return Promise.reject(error);
     },
